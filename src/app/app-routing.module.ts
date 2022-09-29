5cdf25a630f162be371140685579208940e67b1c
Tyrtychko Roman <tyrtychko.r404man@gmail.com>
2022-09-29 19:58:56 +0500

    Layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/pages/cart/cart.component';
+import { ConfirmationComponent } from './components/pages/confirmation/confirmation.component';
+import { DetailsComponent } from './components/pages/details/details.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { SuccessComponent } from './components/pages/success/success.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'success', component: SuccessComponent },
+  { path: 'confirmation', component: ConfirmationComponent },
+  { path: 'details', component: DetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', component: MainComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CartComponent } from './components/pages/cart/cart.component';
+import { DetailsComponent } from './components/pages/details/details.component';
+import { CardComponent } from './components/card/card.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { OrderComponent } from './components/order/order.component';
+import { ConfirmationComponent } from './components/pages/confirmation/confirmation.component';
+import { SuccessComponent } from './components/pages/success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    DetailsComponent,
+    CardComponent,
+    MainComponent,
+    OrderComponent,
+    ConfirmationComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
